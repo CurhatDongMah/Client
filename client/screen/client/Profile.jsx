3ce069d008cc160d9e4c7c84d972692db6b75c61
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-02-20 03:08:04 +0700

    Chat Implementation Added: dependencies: firebase, react-firebase-hooks, react-native-gifted-chat Added: giftedchat in client/screen/client/ChatRoom.jsx Added: helper file for firebase usage Added: TouchableOpacity to navigate to chat room

--- client/screen/client/Profile.jsx
+++ client/screen/client/Profile.jsx
@@ -33,7 +33,7 @@ export default function Profile({ navigation }) {
     },
   ];
 
-  const Item = ({ client }) => (
+  const Item = ({ therapist }) => (
     <View style={{ width: widthWindow * 9 / 10 }}>
       <View style={tailwind('flex flex-row mt-4 rounded-xl py-4 bg-gray-50 justify-start')}>
         <View style={tailwind('px-5 flex items-center justify-center')}>
@@ -48,7 +48,7 @@ export default function Profile({ navigation }) {
           <Text 
             numberOfLines={1}
             ellipsizeMode='clip'
-            style={tailwind('w-36 text-base text-gray-500')}>{client.fullName}</Text>
+            style={tailwind('w-36 text-base text-gray-500')}>{therapist.fullName}</Text>
           <View style={tailwind('flex flex-row items-center')}>
             <Ionicons style={tailwind('mr-1 text-yellow-500 text-base')} name='star'/>
             <Ionicons style={tailwind('mr-1 text-yellow-500 text-base')} name='star'/>
@@ -59,7 +59,7 @@ export default function Profile({ navigation }) {
           <Text style={tailwind('text-gray-400')}>Jakarta</Text>
         </View>
         <View style={tailwind('mx-2 border-l border-gray-200 px-3')}>
-          <Text style={tailwind('text-gray-500')}>IDR {client.price}/h</Text>
+          <Text style={tailwind('text-gray-500')}>IDR {therapist.price}/h</Text>
           <TouchableOpacity
             onPress={() => navigation.navigate('Detail')}
             style={tailwind('items-center mt-2 py-1 px-4 rounded-lg bg-gray-100 border border-r border-green-400')}>
@@ -68,7 +68,7 @@ export default function Profile({ navigation }) {
             >Detail</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => navigation.navigate('ChatRoom', { client })} 
+            onPress={() => navigation.navigate('ChatRoom', { therapist })} 
             style={tailwind('items-center mt-2 py-1 px-4 rounded-lg bg-green-500 border border-r border-green-400')}>
             <Text 
               style={tailwind('text-gray-100')}
@@ -80,7 +80,7 @@ export default function Profile({ navigation }) {
   );
 
   const renderItem = ({ item }) => (
-    <Item client={item} />
+    <Item therapist={item} />
   );
   return (
     <SafeAreaView style={tailwind('flex-1 items-center bg-white')}>
