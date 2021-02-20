9c2582a88930d8a79c2b410ce92da556e340faf8
ariftoteles <arif.santoso4@gmail.com>
2021-02-20 15:18:15 +0700

    Create form edit therapist

---
+++ client/screen/therapist/FormEdit.jsx
@@ -0,0 +1,198 @@
+import React, { useState, useEffect } from 'react'
+import { Text, TextInput, TouchableOpacity, View, SafeAreaView, useWindowDimensions } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import tailwind from 'tailwind-rn'
+import { Datepicker } from '@ui-kitten/components'
+import { Radio, RadioGroup} from '@ui-kitten/components'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+
+export default function SignupForm({ navigation }) {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [birthDate, setBirthDate] = useState(new Date())
+  const [value, setValue] = useState({})
+  const [error, setError] = useState({})
+  const widthWindow = useWindowDimensions().width
+  useEffect(() => {
+    setValue({...value, birthDate: birthDate})
+  }, [birthDate])
+  useEffect(() => {
+    selectedIndex === 1 ? setValue({...value, gender: 'male'}) : setValue({...value, gender: 'female'})
+  }, [selectedIndex])
+  const handleChange = (text, name) => {
+    setError({})
+    setValue({ ...value, [name]: text})
+  }
+  const handleSubmit = () => {
+    if (!value.fullName) setError({...error, fullName: 'Required'})
+    else if (!value.email) setError({...error, email: 'Required'})
+    else if (!value.password) setError({...error, password: 'Required'})
+    else if (!value.photoUrl) setError({...error, photoUrl: 'Required'})
+    else if (!value.birthDate) setError({...error, birthDate: 'Required'})
+    else if (!value.city) setError({...error, city: 'Required'})
+    else if (!value.licenseUrl) setError({...error, licenseUrl: 'Required'})
+    else if (!value.price) setError({...error, price: 'Required'})
+    else if (!value.about) setError({...error, about: 'Required'})
+    else navigation.navigate('TherapistPage')
+    // else console.log(value);
+  }
+  return (
+    <SafeAreaView style={tailwind('flex-1 items-center justify-center bg-white')}>
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        style={{ width: widthWindow * 8 / 10, marginTop: 40 }}
+      >
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>FULL NAME</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'fullName')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.fullName ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.fullName}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>EMAIL</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'email')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.email ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.email}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>PASSWORD</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'password')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+            secureTextEntry={true}
+          ></TextInput>
+          {
+            error.password ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.password}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>PHOTO URL</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'photoUrl')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.photoUrl ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.photoUrl}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>BIRTH DATE</Text>
+          <Datepicker
+            date={birthDate}
+            onSelect={nextDate => setBirthDate(nextDate)}
+          />
+          {
+            error.birthDate ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.birthDate}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>CITY</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'city')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.city ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.city}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>LICENSE URL</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'licenseUrl')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.licenseUrl ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.licenseUrl}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>PRICE</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'price')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.price ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.price}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>ABOUT</Text>
+          <TextInput
+            onChangeText={(text) => handleChange(text, 'about')}
+            style={tailwind('px-3 py-2 bg-white text-xl text-gray-500 border-b border-green-400 rounded-xl')}
+          ></TextInput>
+          {
+            error.about ? (
+              <View style={tailwind('flex flex-row items-center')}>
+                <Ionicons style={tailwind('mx-1 text-red-400 text-lg')} name='warning-outline'/>
+                <Text style={tailwind('text-sm text-red-400')}>{error.about}</Text> 
+              </View>
+            ): <Text></Text>
+          }
+        </View>
+        <RadioGroup
+          selectedIndex={selectedIndex}
+          onChange={index => setSelectedIndex(index)}>
+          <Radio status='success'>Female</Radio>
+          <Radio status='success'>Male</Radio>
+        </RadioGroup>
+        <TouchableOpacity
+          onPress={() => handleSubmit()} 
+          style={tailwind('items-center py-3 mt-8 rounded-full bg-green-400')}>
+          <Text 
+            style={tailwind('text-xl text-gray-100')}
+          >SUBMIT</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
--- client/screen/therapist/Home.jsx
+++ client/screen/therapist/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import Profile from './Profile'
+import FormEdit from './FormEdit'
 
 const Stack = createStackNavigator()
 
 export default function Home() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Profile" component={Profile}/>
+      <Stack.Screen name="TherapistEdit" component={FormEdit}/>
     </Stack.Navigator>
   );
 }
