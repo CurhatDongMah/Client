22cb19b90651a7185b53b5b353decc2782062844
ObedAdhi <obedadhinatapraya@yahoo.com>
2021-02-21 15:56:25 +0700

    Chat Implentation Added: Chat Room therapist side Added: temporary get all client in history screen/client/ChatRoom Added: Function: get all client

--- client/screen/therapist/History.jsx
+++ client/screen/therapist/History.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, Text, View, Image, FlatList, TouchableOpacity, useWindowDimensions } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useDispatch, useSelector } from 'react-redux';
 import tailwind from 'tailwind-rn';
+import { getClients } from '../../store/actions/client';
 
 export default function ListHistory({ navigation }) {
   const widthWindow = useWindowDimensions().width
@@ -39,15 +41,21 @@ export default function ListHistory({ navigation }) {
       title: 'Third Item',
     },
   ];
+  const dispatch = useDispatch()
+  const { clients, error, loading } = useSelector(state => state.client)
 
-  const Item = ({ title }) => (
+  useEffect(() => {
+    dispatch(getClients())
+  }, [])
+
+  const Item = ({ client }) => (
     <View style={{ width: widthWindow * 9 / 10 }}>
       <View style={tailwind('flex flex-row mt-4 rounded-xl py-4 bg-gray-50 justify-start')}>
         <View style={tailwind('px-5 flex items-center justify-center')}>
           <Image 
             style={tailwind('w-12 h-12 rounded-full')}
             source={{
-              uri: 'https://picsum.photos/id/237/200/300'
+              uri: client.photoUrl
             }}
           />
         </View>
@@ -55,26 +63,33 @@ export default function ListHistory({ navigation }) {
           <Text 
             numberOfLines={1}
             ellipsizeMode='clip'
-            style={tailwind('text-base text-gray-500')}>Mama Dedeh Alexandersdsdsdadsas</Text>
+            style={tailwind('text-base text-gray-500')}>{client.fullName}</Text>
           <Text style={tailwind('text-gray-500')}>27/02/2021</Text>
           <Text style={tailwind('text-gray-500')}>09.00 : 10.00 pm</Text>
-          <Text style={tailwind('text-gray-400')}>Jakarta</Text>
+          <Text style={tailwind('text-gray-400')}>{client.city}</Text>
+          <TouchableOpacity
+            onPress={async () => navigation.navigate('ChatRoom', { client })} 
+            style={tailwind('items-center mt-2 py-1 px-4 rounded-lg bg-green-500 border border-r border-green-400')}>
+            <Text 
+              style={tailwind('text-gray-100')}
+            >Chat</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
   );
 
   const renderItem = ({ item }) => (
-    <Item title={item.title} />
+    <Item client={item} />
   );
   return (
     <SafeAreaView style={tailwind('flex-1 items-center bg-white')}>
       <View style={tailwind('pt-12')}>
         <Text style={tailwind('py-2 text-lg text-gray-400 tracking-wider')}>LIST HISTORY</Text>
         <FlatList
-          data={DATA}
+          data={clients}
           renderItem={renderItem}
-          keyExtractor={item => item.id}
+          keyExtractor={item => item.id.toString()}
           showsVerticalScrollIndicator={false}
         />
       </View>
