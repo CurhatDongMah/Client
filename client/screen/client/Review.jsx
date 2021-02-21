803dd05657b20bc023af5aa919f77e7842d3739c
ariftoteles <arif.santoso4@gmail.com>
2021-02-21 18:11:41 +0700

    Add feature refresh on profile screen

--- client/screen/client/Review.jsx
+++ client/screen/client/Review.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { SafeAreaView, Text, View, TextInput, TouchableOpacity, useWindowDimensions } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import tailwind from 'tailwind-rn';
 
 export default function Review({ navigation }) {
   const widthWindow = useWindowDimensions().width
+  const [rating, setRating] = useState(0)
+  const [review, setReview] = useState('')
+  const { therapistDetail } = useSelector(state => state.client)
   return (
     <SafeAreaView style={tailwind('flex-1 items-center justify-center bg-white')}>
       <View style={{ width: widthWindow * 9 / 10}}>
         <View style={tailwind('mt-5')}>
           <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>SHARE YOUR EXPERIENCE</Text>
           <TextInput
+            onChange={(text) => setReview(text)}
             multiline={true}
             numberOfLines={5}
             style={tailwind('py-2 bg-gray-50 text-xl text-gray-500 border-b border-green-400 rounded-xl')}
