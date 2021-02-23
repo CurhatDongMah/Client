4883238aad6e0700ae79c9e5facfcf2d6d7a2e9b
ariftoteles <arif.santoso4@gmail.com>
2021-02-23 12:08:13 +0700

    Redesign accout secreen

--- client/screen/client/Logout.jsx
+++ client/screen/client/Logout.jsx
@@ -1,21 +1,80 @@
-import React from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
-import * as SecureStore from 'expo-secure-store';
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Text, TextInput, TouchableOpacity, View, SafeAreaView, useWindowDimensions, Image } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
 import tailwind from 'tailwind-rn'
+import { Datepicker } from '@ui-kitten/components'
+import { Radio, RadioGroup} from '@ui-kitten/components'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import { editClient } from '../../store/actions/client'
+import * as SecureStore from 'expo-secure-store';
 
 export default function Logout({ navigation }) {
+  const widthWindow = useWindowDimensions().width
+  const { client, onGoingOrders } = useSelector(state => state.client)
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <TouchableOpacity
-        onPress={async () => {
-          await SecureStore.deleteItemAsync('access_token')
-          navigation.navigate('Signin')
-        }} 
-        style={tailwind('w-80 items-center py-3 mt-8 rounded-full bg-red-400')}>
-        <Text 
-          style={tailwind('text-xl text-gray-100 tracking-wider')}
-        >LOGOUT</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={tailwind('flex-1 items-center justify-center bg-white')}>
+    <ScrollView 
+      showsVerticalScrollIndicator={false}
+      style={{ width: widthWindow * 8 / 10, marginTop: 40}}
+    >
+      <View style={tailwind('flex flex-row pt-16 pb-8 w-full justify-start border-b-4 border-green-400')}>
+        <View>
+          <Image 
+            style={tailwind('w-20 h-20 rounded-full')}
+            source={{
+              uri: client.photoUrl
+            }}
+          />
+        </View>
+        <View style={tailwind('flex items-start justify-center px-6')}>
+          <Text style={tailwind('text-2xl text-gray-600')}>{ client.fullName }</Text>
+          <Text 
+            onPress={() => navigation.navigate('ClientEdit')}
+            style={tailwind('text-green-400 text-base')}
+          >Edit Profile</Text>
+          <Text 
+            onPress={async () => {
+              await SecureStore.deleteItemAsync('access_token')
+              navigation.navigate('Signin')
+            }} 
+            style={tailwind('text-red-400 text-base mt-2')}
+          >Logout</Text>
+        </View>
+      </View>
+      <View style={tailwind('mt-5')}>
+        <View style={tailwind('mt-2')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>Email</Text>
+          <Text 
+            style={tailwind('py-2 text-base text-gray-500 border-b border-gray-100')}
+          >arif@mail.com</Text>
+        </View>
+        <View style={tailwind('mt-2')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>Age</Text>
+          <Text 
+            style={tailwind('py-2 text-base text-gray-500 border-b border-gray-100')}
+          >17 years old</Text>
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>Gender</Text>
+          <Text 
+            style={tailwind('py-2 text-base text-gray-500 border-b border-gray-100')}
+          >male</Text>
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>City</Text>
+          <Text 
+            style={tailwind('py-2 text-base text-gray-500 border-b border-gray-100')}
+          >London</Text>
+        </View>
+        <View style={tailwind('mt-5')}>
+          <Text style={tailwind('text-lg text-gray-400 tracking-wider')}>About</Text>
+          <Text 
+            style={tailwind('py-2 text-base text-gray-500 border-b border-gray-100')}
+          >Lalala</Text>
+        </View>
+      </View>
+    </ScrollView>
+  </SafeAreaView>
   )
 }
