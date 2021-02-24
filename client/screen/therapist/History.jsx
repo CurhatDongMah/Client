32f57504b23ebbfcaafec9f21f46ae5d11458f4d
ariftoteles <arif.santoso4@gmail.com>
2021-02-24 20:32:53 +0700

    Refactor component history therapist-ui

--- client/screen/therapist/History.jsx
+++ client/screen/therapist/History.jsx
@@ -16,6 +16,7 @@ import tailwind from 'tailwind-rn';
 import twoDigitFormat from '../../helpers/twoDigitFormat';
 import { getClients } from '../../store/actions/client';
 import { getHistoryTherapist } from '../../store/actions/therapist'
+import TherapistHistoryCard from '../../components/TherapistHistoryCard'
 const wait = (timeout) => {
   return new Promise(resolve => setTimeout(resolve, timeout));
 }
@@ -36,44 +37,6 @@ export default function ListHistory({ navigation }) {
     useEffect(() => {
     dispatch(getClients())
   }, [])
-
-  // const Item = ( history ) => (
-  //   <View style={{ width: widthWindow * 9 / 10 }}>
-  //     <View style={tailwind('flex flex-row my-2 rounded-xl py-2 bg-gray-100 justify-start')}>
-  //       <View style={tailwind('px-5 flex items-center justify-center')}>
-  //         <Image 
-  //           style={tailwind('w-12 h-12 rounded-full')}
-  //           source={{
-  //             uri: history.Client.photoUrl
-  //           }}
-  //         />
-  //       </View>
-  //       <View style={tailwind('flex items-start justify-center px-2')}>
-  //         <Text 
-  //           numberOfLines={1}
-  //           ellipsizeMode='clip'
-  //           style={tailwind('w-24 text-lg text-gray-500')}>{history.title.Client.fullName}</Text>
-  //         <Text style={tailwind('text-gray-400 text-base')}>{history.title.Client.city}</Text>
-  //         <TouchableOpacity
-  //           onPress={async () => navigation.navigate('Messages', { screen: 'ChatRoom', params: {client: history.title.Client }})} 
-  //           style={tailwind('items-center my-1 px-6 rounded border border-r border-green-400')}>
-  //           <Text 
-  //             style={tailwind('text-green-400')}
-  //           >Chat</Text>
-  //         </TouchableOpacity>
-  //       </View>
-  //       <View style={tailwind('mx-2 border-l border-gray-200 px-3 flex justify-center')}>
-  //         <Text style={tailwind('text-gray-500 text-base')}>Date: { `${new Date(history.title.createdAt).getDate()}/${new Date(history.title.createdAt).getMonth()+1}/${new Date(history.title.createdAt).getFullYear()}`}</Text>
-  //         <Text style={tailwind('text-gray-500 text-base')}>Start at: { `${new Date(history.title.createdAt).getHours()} : ${new Date(history.title.createdAt).getMinutes()}`}</Text>
-  //         <Text style={tailwind('text-gray-500 text-base')}>Duration: { history.title.totalHour } Hour</Text>
-  //       </View>
-  //     </View>
-  //   </View>
-  // );
-
-  // const renderItem = ({ item }) => (
-  //   <Item title={item} />
-  // );
 
   if (error) {
     return (
@@ -110,60 +73,47 @@ export default function ListHistory({ navigation }) {
               historiesTherapist.length ? (
                 historiesTherapist.map(history => {
                   return (
-                    <View key={history.id} style={{ width: widthWindow * 9 / 10 }}>
-                      <View style={tailwind('flex flex-row my-2 rounded-xl py-2 bg-gray-100 justify-start')}>
-                        <View style={tailwind('px-5 flex items-center justify-center')}>
-                          <Image 
-                            style={tailwind('w-12 h-12 rounded-full')}
-                            source={{
-                              uri: history.Client.photoUrl
-                            }}
-                          />
-                        </View>
-                        <View style={tailwind('flex items-start justify-center px-2')}>
-                          <Text 
-                            numberOfLines={1}
-                            ellipsizeMode='clip'
-                            style={tailwind('w-24 text-lg text-gray-500')}>{history.Client.fullName}</Text>
-                          <Text style={tailwind('text-gray-400 text-base')}>{history.Client.city}</Text>
-                          <TouchableOpacity
-                            onPress={async () => navigation.navigate('Messages', { screen: 'ChatRoom', params: {client: history.Client }})} 
-                            style={tailwind('items-center my-1 px-6 rounded border border-r border-green-400')}>
-                            <Text 
-                              style={tailwind('text-green-400')}
-                            >Chat</Text>
-                          </TouchableOpacity>
-                        </View>
-                        <View style={tailwind('mx-2 border-l border-gray-200 px-3 flex justify-center')}>
-                          <Text style={tailwind('text-gray-500 text-base')}>Date: { `${twoDigitFormat(new Date(history.createdAt).getDate())}/${twoDigitFormat(new Date(history.createdAt).getMonth()+1)}/${new Date(history.createdAt).getFullYear()}`}</Text>
-                          <Text style={tailwind('text-gray-500 text-base')}>Start at: { `${twoDigitFormat(new Date(history.createdAt).getHours())} : ${twoDigitFormat(new Date(history.createdAt).getMinutes())}`}</Text>
-                          <Text style={tailwind('text-gray-500 text-base')}>Duration: { history.totalHour } Hour</Text>
-                        </View>
-                      </View>
-                    </View>
+                    <TherapistHistoryCard 
+                      key={history.id}
+                      history={history}
+                      handleChat={async () => navigation.navigate('Messages', { screen: 'ChatRoom', params: {client: history.Client }})}
+                    />
+                    // <View key={history.id} style={{ width: widthWindow * 9 / 10 }}>
+                    //   <View style={tailwind('flex flex-row my-2 rounded-xl py-2 bg-gray-100 justify-start')}>
+                    //     <View style={tailwind('px-5 flex items-center justify-center')}>
+                    //       <Image 
+                    //         style={tailwind('w-12 h-12 rounded-full')}
+                    //         source={{
+                    //           uri: history.Client.photoUrl
+                    //         }}
+                    //       />
+                    //     </View>
+                    //     <View style={tailwind('flex items-start justify-center px-2')}>
+                    //       <Text 
+                    //         numberOfLines={1}
+                    //         ellipsizeMode='clip'
+                    //         style={tailwind('w-24 text-lg text-gray-500')}>{history.Client.fullName}</Text>
+                    //       <Text style={tailwind('text-gray-400 text-base')}>{history.Client.city}</Text>
+                    //       <TouchableOpacity
+                    //         onPress={async () => navigation.navigate('Messages', { screen: 'ChatRoom', params: {client: history.Client }})} 
+                    //         style={tailwind('items-center my-1 px-6 rounded border border-r border-green-400')}>
+                    //         <Text 
+                    //           style={tailwind('text-green-400')}
+                    //         >Chat</Text>
+                    //       </TouchableOpacity>
+                    //     </View>
+                    //     <View style={tailwind('mx-2 border-l border-gray-200 px-3 flex justify-center')}>
+                    //       <Text style={tailwind('text-gray-500 text-base')}>Date: { `${twoDigitFormat(new Date(history.createdAt).getDate())}/${twoDigitFormat(new Date(history.createdAt).getMonth()+1)}/${new Date(history.createdAt).getFullYear()}`}</Text>
+                    //       <Text style={tailwind('text-gray-500 text-base')}>Start at: { `${twoDigitFormat(new Date(history.createdAt).getHours())} : ${twoDigitFormat(new Date(history.createdAt).getMinutes())}`}</Text>
+                    //       <Text style={tailwind('text-gray-500 text-base')}>Duration: { history.totalHour } Hour</Text>
+                    //     </View>
+                    //   </View>
+                    // </View>
                   ) 
                 })
               ) : <Text style={tailwind('py-4 text-lg text-gray-500 tracking-wider')}>No history order</Text>
             }
           </ScrollView>
-          {/* {
-            historiesTherapist.length 
-              ? (
-                <FlatList
-                  data={historiesTherapist}
-                  renderItem={renderItem}
-                  keyExtractor={item => item.id.toString()}
-                  showsVerticalScrollIndicator={false}
-                  refreshControl={
-                    <RefreshControl
-                    refreshing={refreshing}
-                    onRefresh={onRefresh}
-                    />
-                  }
-                />
-              ) 
-              : <Text>No History</Text>
-          } */}
       </SafeAreaView>
     )
   }
