fea2530cbb868fd0c40a8f16ed422584ee46aadc
ariftoteles <arif.santoso4@gmail.com>
2021-02-24 10:21:54 +0700

    Handle navigation webview to react

--- client/store/actions/therapist.js
+++ client/store/actions/therapist.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import * as SecureStore from 'expo-secure-store';
-// const baseUrl = 'http://192.168.43.213:3000' //arif
-const baseUrl = 'http://192.168.0.10:3000' //obed
+const baseUrl = 'http://192.168.43.213:3000' //arif
+// const baseUrl = 'http://192.168.0.10:3000' //obed
 // const baseUrl = 'http://192.168.8.104:3000' //riva
 
 
